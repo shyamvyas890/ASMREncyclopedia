1d77777ab1bf9efa9147e11bb4c6e93091d035a1
Shyam Vyas <44505988+shyamvyas890@users.noreply.github.com>
2023-12-26 00:50:11 -0800

    Defined functions to deal with clicking like or dislike button

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -1,27 +1,26 @@
 import React, { useState } from 'react';
 
 const PostComponent = (props) =>{
-    function getYouTubeVideoId(url) {
-        try {
-          const youtubeUrl = new URL(url);
-          if ((youtubeUrl.hostname === 'www.youtube.com' || youtubeUrl.hostname === 'youtu.be') && youtubeUrl.searchParams.has('v')) {
-            return youtubeUrl.searchParams.get('v');
-          }
-        } catch (error) {
-          console.error('Error extracting YouTube video ID:', error);
-        }
-        return null;
+    
+
+    const handleLike = async () => {
+
+
+    }
+
+    const handleDislike= async ()=>{
+
     }
 
     return (
         <div>
             <h5>{props.username}</h5>
             <h6>{props.title}</h6>
-            <iframe width="420" height="315"
-                src={`https://www.youtube.com/embed/${getYouTubeVideoId(props.link)}`}>
+            <iframe width="420" height="315" title= "Title" allow="fullscreen;"
+                src={`https://www.youtube.com/embed/${props.id}`}>
             </iframe>
-            <button>Like </button>
-            <button>Dislike</button>
+            <button onClick={handleLike}>Like </button>
+            <button onClick={handleDislike}>Dislike</button>
         </div>
     );
 }
