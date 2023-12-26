b782152c87024e5343e25079213bb0fc9d17745f
Shyam Vyas <44505988+shyamvyas890@users.noreply.github.com>
2023-12-25 16:20:08 -0800

    Updated login.jsx to get shared states from parent component

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -2,42 +2,36 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-const LoginComponent = () => {
-  const [username, setUsername] = useState('');
+const LoginComponent = (props) => {
   const [password, setPassword] = useState('');
-  const [isLoggedIn, setIsLoggedIn]= useState(false);
   const handleLogin = async (e) => {
     e.preventDefault();
-
     try {
       const response = await axios.post('http://localhost:3001/login', {
-        username,
+        username:props.username,
         password
       });
-
       console.log('Login successful. Token:', response.data.token);
       localStorage.setItem("token", response.data.token);
-      setIsLoggedIn(true);
+      props.setIsLoggedIn(true);
 
       // You may want to store the token in state or local storage for authentication
     } catch (error) {
       console.log('Login error:', error);
     }
   };
-
   const tokenVerify= async (e) => {
-
     const theToken= localStorage.getItem("token");
     if(theToken){
         try{
             const response= await axios.get(`http://localhost:3001/verify-token/${theToken}`)
             console.log(response);
             if(response.data.username){
-                setUsername(response.data.username)
-                setIsLoggedIn(true);
+                props.setUsername(response.data.username)
+                props.setIsLoggedIn(true);
             }
             else {
-                setIsLoggedIn(false);
+                props.setIsLoggedIn(false);
             }
         }
 
@@ -46,29 +40,27 @@ const LoginComponent = () => {
         }
     }
     else{
-        setIsLoggedIn(false);
+        props.setIsLoggedIn(false);
     }
-
   }
 
   React.useEffect(()=>{
     tokenVerify();
-    
   }, []);
 
   const handleLogout = async (e) => {
     await axios.post(`http://localhost:3001/logout/${localStorage.getItem("token")}`);
     localStorage.removeItem("token");
-    setIsLoggedIn(false);
-    setUsername("");
+    props.setIsLoggedIn(false);
+    props.setUsername("");
     setPassword("");
   }
 
   return (
     <div>
-        {isLoggedIn?(
+        {props.isLoggedIn?(
             <div>
-            <h1>Welcome, {username}</h1>
+            <h1>Welcome, {props.username}!</h1>
             <button onClick={handleLogout}>Logout</button>
             </div>
 
@@ -77,7 +69,7 @@ const LoginComponent = () => {
         <form onSubmit={handleLogin}>
             <label>
             Username:
-            <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+            <input type="text" value={props.username} onChange={(e) => props.setUsername(e.target.value)} />
             </label>
             <br />
             <label>
@@ -93,5 +85,4 @@ const LoginComponent = () => {
     </div>
   );
 };
-
 export default LoginComponent;
