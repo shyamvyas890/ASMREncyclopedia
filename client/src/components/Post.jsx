5a682c6ab20b50ebda93ab73d44362547d6458b2
Shyam Vyas <44505988+shyamvyas890@users.noreply.github.com>
2023-12-26 23:01:25 -0800

    Finished implementing the like and dislike button of video posts

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -1,26 +1,142 @@
-import React, { useState } from 'react';
-
+import React from 'react';
+import axios from 'axios';
 const PostComponent = (props) =>{
-    
+    const handleLike = async (e) => {
+        e.preventDefault();
+        if(props.rating===0){
+          const axiosResponse = await axios.post(`http://localhost:3001/video-rating/${props.VideoPostId}`, {UserId: props.userIdOfCurrentUser, LikeStatus: true})
+          props.setUserRatings(function(prev){
+            let newUserRatings= [];
+            for(let i=0; i<prev.length;i++) {
+              if(i===props.index){
+                newUserRatings.push(1);
+              }
+              else {
+                newUserRatings.push(prev[i]);
+              }
+            }
+            return newUserRatings;
+          })      
+        }
+        else if (props.rating===-1){
+          const getLikeDislikeId= await axios.get(`http://localhost:3001/video-rating`, {params: {UserId: props.userIdOfCurrentUser, VideoPostId: props.VideoPostId}});
+          const LikeDislikeId= getLikeDislikeId.data[0].LikeDislikeId;
+          const deleteOldRating = await axios.delete('http://localhost:3001/video', {params: {LikeDislikeId}});
+          const addUpdatedRating = await axios.post(`http://localhost:3001/video-rating/${props.VideoPostId}`, {UserId: props.userIdOfCurrentUser, LikeStatus: true})
+          props.setUserRatings(function(prev){
+            let newUserRatings= [];
+            for(let i=0; i<prev.length;i++) {
+              if(i===props.index){
+                newUserRatings.push(1);
+              }
+              else {
+                newUserRatings.push(prev[i]);
+              }
+            }
+            return newUserRatings;
+          }) 
+          
+        }
+        else if (props.rating===1){
+          const getLikeDislikeId= await axios.get(`http://localhost:3001/video-rating`, {params: {UserId: props.userIdOfCurrentUser, VideoPostId: props.VideoPostId}});
+          const LikeDislikeId= getLikeDislikeId.data[0].LikeDislikeId;
+          const deleteOldRating = await axios.delete('http://localhost:3001/video', {params: {LikeDislikeId}});
+          props.setUserRatings(function(prev){
+            let newUserRatings= [];
+            for(let i=0; i<prev.length;i++) {
+              if(i===props.index){
+                newUserRatings.push(0);
+              }
+              else {
+                newUserRatings.push(prev[i]);
+              }
+            }
+            return newUserRatings;
+          }) 
 
-    const handleLike = async () => {
+        }
+    }
 
+    const handleDislike= async (e)=>{
+        e.preventDefault();
+        if(props.rating===0){
+          const axiosResponse = await axios.post(`http://localhost:3001/video-rating/${props.VideoPostId}`, {UserId: props.userIdOfCurrentUser, LikeStatus: false})
+          props.setUserRatings(function(prev){
+            let newUserRatings= [];
+            for(let i=0; i<prev.length;i++) {
+              if(i===props.index){
+                newUserRatings.push(-1);
+              }
+              else {
+                newUserRatings.push(prev[i]);
+              }
+            }
+            return newUserRatings;
+          })      
+        }
+        else if (props.rating===1){
+          const getLikeDislikeId= await axios.get(`http://localhost:3001/video-rating`, {params: {UserId: props.userIdOfCurrentUser, VideoPostId: props.VideoPostId}});
+          const LikeDislikeId= getLikeDislikeId.data[0].LikeDislikeId;
+          const deleteOldRating = await axios.delete('http://localhost:3001/video', {params: {LikeDislikeId}});
+          const addUpdatedRating = await axios.post(`http://localhost:3001/video-rating/${props.VideoPostId}`, {UserId: props.userIdOfCurrentUser, LikeStatus: false})
+          props.setUserRatings(function(prev){
+            let newUserRatings= [];
+            for(let i=0; i<prev.length;i++) {
+              if(i===props.index){
+                newUserRatings.push(-1);
+              }
+              else {
+                newUserRatings.push(prev[i]);
+              }
+            }
+            return newUserRatings;
+          }) 
+          
+        }
+        else if (props.rating===-1){
+          const getLikeDislikeId= await axios.get(`http://localhost:3001/video-rating`, {params: {UserId: props.userIdOfCurrentUser, VideoPostId: props.VideoPostId}});
+          const LikeDislikeId= getLikeDislikeId.data[0].LikeDislikeId;
+          const deleteOldRating = await axios.delete('http://localhost:3001/video', {params: {LikeDislikeId}});
+          props.setUserRatings(function(prev){
+            let newUserRatings= [];
+            for(let i=0; i<prev.length;i++) {
+              if(i===props.index){
+                newUserRatings.push(0);
+              }
+              else {
+                newUserRatings.push(prev[i]);
+              }
+            }
+            return newUserRatings;
+          }) 
+
+        }
 
     }
 
-    const handleDislike= async ()=>{
 
-    }
+    const highlightLikeButtonRating= {
+        color: props.rating===1? "white":"black",
+        backgroundColor: props.rating===1? "black": "white",
+        border: '1px solid #153e59', // Set your desired border color
+        borderRadius: '4px'
+    };
+    const highlightDislikeButtonRating= {
+      color: props.rating===-1? "white":"black",
+      backgroundColor: props.rating===-1? "black": "white",
+      border: '1px solid #153e59', // Set your desired border color
+      borderRadius: '4px'
+  };
 
     return (
         <div>
             <h5>{props.username}</h5>
             <h6>{props.title}</h6>
             <iframe width="420" height="315" title= "Title" allow="fullscreen;"
-                src={`https://www.youtube.com/embed/${props.id}`}>
+                src={`https://www.youtube.com/embed/${props.VideoLinkId}`}>
             </iframe>
-            <button onClick={handleLike}>Like </button>
-            <button onClick={handleDislike}>Dislike</button>
+            <button onClick={handleLike} style={highlightLikeButtonRating}>Like </button>
+            <button onClick={handleDislike} style={highlightDislikeButtonRating}>Dislike</button>
         </div>
     );
 }
