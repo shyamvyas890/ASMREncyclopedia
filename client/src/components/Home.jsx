8de76092541fbdd77bf71224765db60b3fd10573
Shyam Vyas <44505988+shyamvyas890@users.noreply.github.com>
2023-12-28 23:57:16 -0800

    Added like and dislike and removing posts changes to home component

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,94 +2,81 @@ import React, { useState } from 'react';
 import LoginComponent from './Login';
 import PostComponent from './Post';
 import axios from 'axios';
+import AddVideoPostComponent from './AddVideoPost';
 const HomeComponent= () => {
     const [username, setUsername] = useState('');
     const [userIdOfCurrentUser, setUserIdOfCurrentUser]= useState(null);
     const [isLoggedIn, setIsLoggedIn]= useState(false);
-    const [videoPosts, setVideoPosts]= useState(null);
-    const [userRatings, setUserRatings]= useState(null);
-    function getYouTubeVideoId(url) {
+    const [videoPostsAndRatings, setVideoPostsAndRatings] = useState(null);
+    const fetchVideoPosts = async ()=>{
         try {
-          const youtubeUrl = new URL(url);
-          if ((youtubeUrl.hostname === 'www.youtube.com' || youtubeUrl.hostname === 'youtu.be') && youtubeUrl.searchParams.has('v')) {
-            return youtubeUrl.searchParams.get('v');
-          }
-        } catch (error) {
-          console.error('Error extracting YouTube video ID:', error);
-        }
-        return null;
-    }
-
-    React.useEffect( () =>{
-        const fetchVideoPosts = async ()=>{
-            try {
-                const thePosts = await axios.get("http://localhost:3001/video");
-                const thePostsData= thePosts.data;
-                for(let i=0;i<thePostsData.length;i++){
-                    try{
-                        const username= await axios.get("http://localhost:3001/users/id", 
-                        {
-                            params:{ UserId:thePostsData[i].UserId }
-                        }
-                        );
-                        thePostsData[i].username=username.data.username;
-                    }
-                    catch(err){
-                        console.log(err);
-                    }
-                }
-                 
-                let feedback=[];
-                let tempHolderOfUserIdOfCurrentUser;
+            const thePosts = await axios.get("http://localhost:3001/video");
+            const thePostsData= thePosts.data;
+            for(let i=0;i<thePostsData.length;i++){
                 try{
-                    const theId= await axios.get("http://localhost:3001/users/id", 
-                        {
-                            params:{ username }
-                        }
+                    const username= await axios.get("http://localhost:3001/users/id", 
+                    {
+                        params:{ UserId:thePostsData[i].UserId }
+                    }
                     );
-                    tempHolderOfUserIdOfCurrentUser=theId.data.id
-                    setUserIdOfCurrentUser(theId.data.id);
+                    thePostsData[i].username=username.data.username;
                 }
                 catch(err){
                     console.log(err);
                 }
-                for(let i=0;i<thePostsData.length;i++){
-                    try{
-                        const rating= await axios.get("http://localhost:3001/video-rating", 
-                        {
-                            params:{ VideoPostId:thePostsData[i].VideoPostId }
-                        }
-                        );
-                        for(let j=0; j<rating.data.length; j=j+1){
-                            if(rating.data[j].UserId===tempHolderOfUserIdOfCurrentUser){
-                                if(rating.data[j].LikeStatus){
-                                    feedback.push(1);
-                                }
-                                else {
-                                    feedback.push(-1);
-                                }
-                                break;
-                            }
-                            else if(j===rating.data.length - 1){
-                                feedback.push(0);
-                            }
-                        }
-                        if(rating.data.length===0){
-                            feedback.push(0);
-                        }    
+            }
+             
+            let feedback=[];
+            let tempHolderOfUserIdOfCurrentUser;
+            try{
+                const theId= await axios.get("http://localhost:3001/users/id", 
+                    {
+                        params:{ username }
                     }
-                    catch(err){
-                        console.log(err)
-                    }
-                }
-                setVideoPosts(thePostsData);
-                setUserRatings(feedback);
-
+                );
+                tempHolderOfUserIdOfCurrentUser=theId.data.id
+                setUserIdOfCurrentUser(theId.data.id);
             }
             catch(err){
                 console.log(err);
             }
+            for(let i=0;i<thePostsData.length;i++){
+                try{
+                    const rating= await axios.get("http://localhost:3001/video-rating", 
+                    {
+                        params:{ VideoPostId:thePostsData[i].VideoPostId }
+                    }
+                    );
+                    for(let j=0; j<rating.data.length; j=j+1){
+                        if(rating.data[j].UserId===tempHolderOfUserIdOfCurrentUser){
+                            if(rating.data[j].LikeStatus){
+                                feedback.push(1);
+                            }
+                            else {
+                                feedback.push(-1);
+                            }
+                            break;
+                        }
+                        else if(j===rating.data.length - 1){
+                            feedback.push(0);
+                        }
+                    }
+                    if(rating.data.length===0){
+                        feedback.push(0);
+                    }    
+                }
+                catch(err){
+                    console.log(err)
+                }
+            }
+            setVideoPostsAndRatings({videoPosts: thePostsData, userRatings:feedback});
+
         }
+        catch(err){
+            console.log(err);
+        }
+    }
+    React.useEffect( () =>{ 
         if(isLoggedIn){
             fetchVideoPosts();
         }
@@ -102,21 +89,28 @@ const HomeComponent= () => {
             isLoggedIn={isLoggedIn}
             setIsLoggedIn={setIsLoggedIn}
             />
-            {(isLoggedIn && videoPosts && userRatings && userIdOfCurrentUser) ? (
-                videoPosts.map((post, index)=>{
-                    return <PostComponent 
-                                key={index}
-                                index={index} 
-                                username={post.username} 
-                                title={post.Title} 
-                                userIdOfCurrentUser= {userIdOfCurrentUser}
-                                VideoLinkId= {post.VideoLinkId}
-                                VideoPostId= {post.VideoPostId}
-                                rating= {userRatings[index]}
-                                setUserRatings={setUserRatings}
-                            />
-                            }
-                        )
+            {(isLoggedIn && videoPostsAndRatings && userIdOfCurrentUser) ? (
+                <>
+                    <h3>Add a new Video!</h3>
+                    <AddVideoPostComponent 
+                    userIdOfCurrentUser={userIdOfCurrentUser}
+                    fetchVideoPosts={fetchVideoPosts}
+                    />
+                    {videoPostsAndRatings.videoPosts.map((post, index)=>(
+                        <PostComponent 
+                            key={index}
+                            index={index} 
+                            username={post.username} 
+                            title={post.Title} 
+                            userIdOfCurrentUser= {userIdOfCurrentUser}
+                            usernameOfCurrentUser= {username}
+                            VideoLinkId= {post.VideoLinkId}
+                            VideoPostId= {post.VideoPostId}
+                            rating= {videoPostsAndRatings.userRatings[index]}
+                            setVideoPostsAndRatings= {setVideoPostsAndRatings}
+                        />
+                    ))}
+                </>
             ):null}
         </div>
     )
