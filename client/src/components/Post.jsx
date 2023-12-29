2d77afb93037ed6316e1443b29ba3d66079e9733
Shyam Vyas <44505988+shyamvyas890@users.noreply.github.com>
2023-12-28 23:58:21 -0800

    Removed repetitive code

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -1,114 +1,84 @@
 import React from 'react';
 import axios from 'axios';
 const PostComponent = (props) =>{
+
+    function changeTheRating(theRating){
+      props.setVideoPostsAndRatings(function(prev){
+        let newUserRatings= [...prev.userRatings];
+        newUserRatings[props.index]=theRating;
+        const newVideoPosts= prev.videoPosts;
+        return {
+          videoPosts:newVideoPosts,
+          userRatings: newUserRatings
+        }
+      })
+    }
     const handleLike = async (e) => {
         e.preventDefault();
         if(props.rating===0){
           const axiosResponse = await axios.post(`http://localhost:3001/video-rating/${props.VideoPostId}`, {UserId: props.userIdOfCurrentUser, LikeStatus: true})
-          props.setUserRatings(function(prev){
-            let newUserRatings= [];
-            for(let i=0; i<prev.length;i++) {
-              if(i===props.index){
-                newUserRatings.push(1);
-              }
-              else {
-                newUserRatings.push(prev[i]);
-              }
-            }
-            return newUserRatings;
-          })      
+          console.log(axiosResponse);
+          changeTheRating(1);      
         }
         else if (props.rating===-1){
           const getLikeDislikeId= await axios.get(`http://localhost:3001/video-rating`, {params: {UserId: props.userIdOfCurrentUser, VideoPostId: props.VideoPostId}});
           const LikeDislikeId= getLikeDislikeId.data[0].LikeDislikeId;
           const deleteOldRating = await axios.delete('http://localhost:3001/video', {params: {LikeDislikeId}});
+          console.log(deleteOldRating)
           const addUpdatedRating = await axios.post(`http://localhost:3001/video-rating/${props.VideoPostId}`, {UserId: props.userIdOfCurrentUser, LikeStatus: true})
-          props.setUserRatings(function(prev){
-            let newUserRatings= [];
-            for(let i=0; i<prev.length;i++) {
-              if(i===props.index){
-                newUserRatings.push(1);
-              }
-              else {
-                newUserRatings.push(prev[i]);
-              }
-            }
-            return newUserRatings;
-          }) 
+          console.log(addUpdatedRating);
+          changeTheRating(1); 
           
         }
         else if (props.rating===1){
           const getLikeDislikeId= await axios.get(`http://localhost:3001/video-rating`, {params: {UserId: props.userIdOfCurrentUser, VideoPostId: props.VideoPostId}});
+          console.log(getLikeDislikeId);
           const LikeDislikeId= getLikeDislikeId.data[0].LikeDislikeId;
           const deleteOldRating = await axios.delete('http://localhost:3001/video', {params: {LikeDislikeId}});
-          props.setUserRatings(function(prev){
-            let newUserRatings= [];
-            for(let i=0; i<prev.length;i++) {
-              if(i===props.index){
-                newUserRatings.push(0);
-              }
-              else {
-                newUserRatings.push(prev[i]);
-              }
-            }
-            return newUserRatings;
-          }) 
+          console.log(deleteOldRating);
 
+          changeTheRating(0)
         }
+    }
+    const handleDelete = async (e) => {
+      e.preventDefault();
+      const deletionResponse = await axios.delete(`http://localhost:3001/video`, {params: {VideoPostId: props.VideoPostId}});
+      console.log(deletionResponse);
+      props.setVideoPostsAndRatings((prev)=>{
+        const newUserRatings= [...prev.userRatings.slice(0, props.index), ...prev.userRatings.slice(props.index+1)];
+        const newVideoPosts= [...prev.videoPosts.slice(0, props.index), ...prev.videoPosts.slice(props.index+1)];
+        return {
+          videoPosts:newVideoPosts,
+          userRatings: newUserRatings
+        }
+      })
     }
 
     const handleDislike= async (e)=>{
         e.preventDefault();
         if(props.rating===0){
           const axiosResponse = await axios.post(`http://localhost:3001/video-rating/${props.VideoPostId}`, {UserId: props.userIdOfCurrentUser, LikeStatus: false})
-          props.setUserRatings(function(prev){
-            let newUserRatings= [];
-            for(let i=0; i<prev.length;i++) {
-              if(i===props.index){
-                newUserRatings.push(-1);
-              }
-              else {
-                newUserRatings.push(prev[i]);
-              }
-            }
-            return newUserRatings;
-          })      
+          console.log(axiosResponse)
+          changeTheRating(-1);   
         }
         else if (props.rating===1){
           const getLikeDislikeId= await axios.get(`http://localhost:3001/video-rating`, {params: {UserId: props.userIdOfCurrentUser, VideoPostId: props.VideoPostId}});
           const LikeDislikeId= getLikeDislikeId.data[0].LikeDislikeId;
+          console.log(getLikeDislikeId)
           const deleteOldRating = await axios.delete('http://localhost:3001/video', {params: {LikeDislikeId}});
+          console.log(deleteOldRating)
           const addUpdatedRating = await axios.post(`http://localhost:3001/video-rating/${props.VideoPostId}`, {UserId: props.userIdOfCurrentUser, LikeStatus: false})
-          props.setUserRatings(function(prev){
-            let newUserRatings= [];
-            for(let i=0; i<prev.length;i++) {
-              if(i===props.index){
-                newUserRatings.push(-1);
-              }
-              else {
-                newUserRatings.push(prev[i]);
-              }
-            }
-            return newUserRatings;
-          }) 
+          console.log(addUpdatedRating)
+          changeTheRating(-1);
           
         }
         else if (props.rating===-1){
           const getLikeDislikeId= await axios.get(`http://localhost:3001/video-rating`, {params: {UserId: props.userIdOfCurrentUser, VideoPostId: props.VideoPostId}});
+          console.log(getLikeDislikeId)
           const LikeDislikeId= getLikeDislikeId.data[0].LikeDislikeId;
           const deleteOldRating = await axios.delete('http://localhost:3001/video', {params: {LikeDislikeId}});
-          props.setUserRatings(function(prev){
-            let newUserRatings= [];
-            for(let i=0; i<prev.length;i++) {
-              if(i===props.index){
-                newUserRatings.push(0);
-              }
-              else {
-                newUserRatings.push(prev[i]);
-              }
-            }
-            return newUserRatings;
-          }) 
+          console.log(deleteOldRating)
+          changeTheRating(0);
 
         }
 
@@ -127,7 +97,6 @@ const PostComponent = (props) =>{
       border: '1px solid #153e59', // Set your desired border color
       borderRadius: '4px'
   };
-
     return (
         <div>
             <h5>{props.username}</h5>
@@ -137,6 +106,7 @@ const PostComponent = (props) =>{
             </iframe>
             <button onClick={handleLike} style={highlightLikeButtonRating}>Like </button>
             <button onClick={handleDislike} style={highlightDislikeButtonRating}>Dislike</button>
+            {props.username === props.usernameOfCurrentUser && <button onClick={handleDelete}>Delete</button>}
         </div>
     );
 }
