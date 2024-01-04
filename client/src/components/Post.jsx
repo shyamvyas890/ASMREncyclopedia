1ed4e001a6de89a69830ea9e84053fade8039a19
Shyam Vyas <44505988+shyamvyas890@users.noreply.github.com>
2024-01-03 19:01:15 -0800

    Cleaned up Post.jsx code

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -17,33 +17,32 @@ const PostComponent = (props) =>{
         e.preventDefault();
         if(props.rating===0){
           const axiosResponse = await axios.post(`http://localhost:3001/video-rating/${props.VideoPostId}`, {UserId: props.userIdOfCurrentUser, LikeStatus: true})
-          console.log(axiosResponse);
           changeTheRating(1);      
         }
         else if (props.rating===-1){
           const getLikeDislikeId= await axios.get(`http://localhost:3001/video-rating`, {params: {UserId: props.userIdOfCurrentUser, VideoPostId: props.VideoPostId}});
           const LikeDislikeId= getLikeDislikeId.data[0].LikeDislikeId;
           const deleteOldRating = await axios.delete('http://localhost:3001/video', {params: {LikeDislikeId}});
-          console.log(deleteOldRating)
           const addUpdatedRating = await axios.post(`http://localhost:3001/video-rating/${props.VideoPostId}`, {UserId: props.userIdOfCurrentUser, LikeStatus: true})
-          console.log(addUpdatedRating);
           changeTheRating(1); 
           
         }
         else if (props.rating===1){
           const getLikeDislikeId= await axios.get(`http://localhost:3001/video-rating`, {params: {UserId: props.userIdOfCurrentUser, VideoPostId: props.VideoPostId}});
-          console.log(getLikeDislikeId);
           const LikeDislikeId= getLikeDislikeId.data[0].LikeDislikeId;
           const deleteOldRating = await axios.delete('http://localhost:3001/video', {params: {LikeDislikeId}});
-          console.log(deleteOldRating);
 
           changeTheRating(0)
         }
     }
     const handleDelete = async (e) => {
       e.preventDefault();
-      const deletionResponse = await axios.delete(`http://localhost:3001/video`, {params: {VideoPostId: props.VideoPostId}});
-      console.log(deletionResponse);
+      try {
+        const deletionResponse = await axios.delete(`http://localhost:3001/video`, {params: {VideoPostId: props.VideoPostId}});
+      }
+      catch(err){
+        console.log(err);
+      }
       props.setVideoPostsAndRatings((prev)=>{
         const newUserRatings= [...prev.userRatings.slice(0, props.index), ...prev.userRatings.slice(props.index+1)];
         const newVideoPosts= [...prev.videoPosts.slice(0, props.index), ...prev.videoPosts.slice(props.index+1)];
@@ -58,26 +57,20 @@ const PostComponent = (props) =>{
         e.preventDefault();
         if(props.rating===0){
           const axiosResponse = await axios.post(`http://localhost:3001/video-rating/${props.VideoPostId}`, {UserId: props.userIdOfCurrentUser, LikeStatus: false})
-          console.log(axiosResponse)
           changeTheRating(-1);   
         }
         else if (props.rating===1){
           const getLikeDislikeId= await axios.get(`http://localhost:3001/video-rating`, {params: {UserId: props.userIdOfCurrentUser, VideoPostId: props.VideoPostId}});
           const LikeDislikeId= getLikeDislikeId.data[0].LikeDislikeId;
-          console.log(getLikeDislikeId)
           const deleteOldRating = await axios.delete('http://localhost:3001/video', {params: {LikeDislikeId}});
-          console.log(deleteOldRating)
           const addUpdatedRating = await axios.post(`http://localhost:3001/video-rating/${props.VideoPostId}`, {UserId: props.userIdOfCurrentUser, LikeStatus: false})
-          console.log(addUpdatedRating)
           changeTheRating(-1);
           
         }
         else if (props.rating===-1){
           const getLikeDislikeId= await axios.get(`http://localhost:3001/video-rating`, {params: {UserId: props.userIdOfCurrentUser, VideoPostId: props.VideoPostId}});
-          console.log(getLikeDislikeId)
           const LikeDislikeId= getLikeDislikeId.data[0].LikeDislikeId;
           const deleteOldRating = await axios.delete('http://localhost:3001/video', {params: {LikeDislikeId}});
-          console.log(deleteOldRating)
           changeTheRating(0);
 
         }
@@ -88,15 +81,15 @@ const PostComponent = (props) =>{
     const highlightLikeButtonRating= {
         color: props.rating===1? "white":"black",
         backgroundColor: props.rating===1? "black": "white",
-        border: '1px solid #153e59', // Set your desired border color
+        border: '1px solid #153e59', 
         borderRadius: '4px'
     };
     const highlightDislikeButtonRating= {
       color: props.rating===-1? "white":"black",
       backgroundColor: props.rating===-1? "black": "white",
-      border: '1px solid #153e59', // Set your desired border color
+      border: '1px solid #153e59', 
       borderRadius: '4px'
-  };
+    };
     return (
         <div>
             <h5>{props.username}</h5>
