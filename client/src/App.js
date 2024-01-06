855e97492e769cc9abb5b81383057c46140c42d0
Shyam Vyas <44505988+shyamvyas890@users.noreply.github.com>
2024-01-06 01:00:00 -0800

    Added the react router link to friends component

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import RegistrationComponent from './components/Register';
 import HomeComponent from './components/Home';
 import NavigationComponent from './components/Navigation';
+import FriendsComponent from './components/Friends';
 function App() {
   const [token, setToken]= React.useState("");
   return (
@@ -13,6 +14,7 @@ function App() {
           <Routes>
             <Route path="/" element={<HomeComponent token= {token} setToken= {setToken}/>}/>
             <Route path="/register" element={<RegistrationComponent token= {token} setToken= {setToken}/>}/>
+            <Route path ="/friends" element= {<FriendsComponent />}/>
           </Routes>
       </BrowserRouter>
       
