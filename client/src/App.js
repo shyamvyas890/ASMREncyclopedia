78d3c683c13cd3ee01ed5e10fef5161f6eaf762b
Ayaz Azhar <68492854+azharayaz99@users.noreply.github.com>
2024-04-07 01:42:28 -0700

    Delete Account & About Us Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
-
 import React from 'react';
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RegistrationComponent from './components/Register';
 import HomeComponent from './components/Home';
 import NavigationComponent from './components/Navigation';
+import ForumComponent from './components/Forum';
+import { AllForumPostComponent } from './components/AllForumPostComponent';
+import { ViewForumPostComponent } from './components/ViewForumPostComponent';
+import { SearchForumPostComponent } from './components/SearchForumPostComponent';
 import FriendsComponent from './components/Friends';
 import SettingsComponent from './components/Settings';
 import VideoPostWithCommentsComponent from './components/VideoPostWithComments';
@@ -11,25 +14,31 @@ import ChatComponent from './components/Chat';
 import ProfilePageComponent from './components/ProfilePage';
 import SearchVideosComponent from './components/SearchVideos';
 import RandomVideoComponent from './components/RandomVideo';
+import AboutUsComponent from './components/AboutUs';
+
 function App() {
-  const [token, setToken]= React.useState("");
+  const [token, setToken] = React.useState("");
   return (
     <div className="App">
       <BrowserRouter>
-          <NavigationComponent token= {token} setToken= {setToken}/>
-          <Routes>
-            <Route path="/" element={<HomeComponent token= {token} setToken= {setToken}/>}/>
-            <Route path="/register" element={<RegistrationComponent token= {token} setToken= {setToken}/>}/>
-            <Route path="/friends" element= {<FriendsComponent />}/>
-            <Route path="/messages" element= {<ChatComponent />}/>
-            <Route path="/settings" element= {<SettingsComponent />} />
-            <Route path="/video/:VideoPostId" element={<VideoPostWithCommentsComponent />} />
-            <Route path="/username/:ProfileUsername" element={<ProfilePageComponent />} />
-            <Route path="/search/videos/:keyword" element={<SearchVideosComponent />} />
-            <Route path="/random" element={<RandomVideoComponent />}/>
-          </Routes>
+        <NavigationComponent token={token} setToken={setToken} />
+        <Routes>
+          <Route path="/" element={<HomeComponent token={token} setToken={setToken} />} />
+          <Route path="/register" element={<RegistrationComponent token={token} setToken={setToken} />} />
+          <Route path="/forums" element={<ForumComponent />} />
+          <Route path="/forumPosts" element={<AllForumPostComponent />} />
+          <Route path="/forumPost/:postID/viewing/:userID/user" element={<ViewForumPostComponent />} />
+          <Route path="/forumPost/search_by/:searchTitle" element={<SearchForumPostComponent />} />
+          <Route path="/friends" element={<FriendsComponent />} />
+          <Route path="/messages" element={<ChatComponent />} />
+          <Route path="/settings" element={<SettingsComponent />} />
+          <Route path="/video/:VideoPostId" element={<VideoPostWithCommentsComponent />} />
+          <Route path="/username/:ProfileUsername" element={<ProfilePageComponent />} />
+          <Route path="/search/videos/:keyword" element={<SearchVideosComponent />} />
+          <Route path="/random" element={<RandomVideoComponent />} />
+          <Route path="/aboutus" element={<AboutUsComponent />} />
+        </Routes>
       </BrowserRouter>
-      
     </div>
   );
 }
